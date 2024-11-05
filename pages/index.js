7e7c93863352c0140601e8f753c1bd0e3e9f3aca
imlobo <aarichk.lo@gmail.com>
2024-11-05 12:22:22 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Popup from '../components/Popup';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js Popup Example</h1>
+      <Popup
+        displayTime={5} // Time in seconds to display the popup
+        displayPeriod={10} // Duration in seconds to show the popup
+        frequency={60} // Frequency in seconds to display the popup again
+        pageUrl="/specific-page" // URL path where the popup should appear
+      />
+    </div>
+  );
+}
